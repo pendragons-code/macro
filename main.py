42ac6631d54bc243ecb40e61621fb2f35a987e2a
Javier <106571756+javiers-code@users.noreply.github.com>
2022-06-05 21:51:45 +0800

    added main.py

---
+++ main.py
@@ -0,0 +1,58 @@
+import pyautogui
+from time import sleep
+from colorama import Fore
+
+pyautogui.FAILSAFE = False
+
+print(Fore.CYAN+"""
+  _
+ | | _____ _   _
+ | |/ / _ \ | | |
+ |   <  __/ |_| |
+ |_|\_\___|\__, |go brrrrr
+           |___/
+""")
+print(Fore.GREEN+"[1] Typewrite")
+print(Fore.GREEN+"[2] Keybinds")
+inputField = input("Please select an option: \n")
+
+if inputField == "1":
+  s = input(Fore.BLUE+"Enter your message: ")
+  x = input(Fore.BLUE+"How many times: ")
+  t = input(Fore.BLUE+"Countdown: ")
+  d = input(Fore.BLUE+"Delay per message: ")
+  howManyX = int(x)
+  tMinus = int(t)
+  delay = int(d)
+
+  print(Fore.YELLOW+"KEY IS STARTING...")
+
+  while(tMinus != 0):
+      print("T-Minus "+str(tMinus))
+      sleep(1)
+      tMinus -= 1   
+
+  for i in range(0, howManyX):
+    sleep(delay)
+    pyautogui.typewrite(s)
+    pyautogui.typewrite("\n")
+
+if inputField == "2":
+  print(Fore.YELLOW+"Have you copied the text? If not, copy it, then come back.")
+  x = input(Fore.BLUE+"How many times: ")
+  t = input(Fore.BLUE+"Countdown: ")
+  d = input(Fore.BLUE+"Delay per message: ")
+  howManyX = int(x)
+  tMinus = int(t)
+  delay = int(d)
+  print(Fore.YELLOW+"KEY IS STARTING...")
+
+  while(tMinus != 0):
+      print("T-Minus "+str(tMinus))
+      sleep(1)
+      tMinus -= 1   
+  
+  for i in range(0, howManyX):
+    sleep(delay)
+    pyautogui.hotkey("ctrl","v")
+    pyautogui.typewrite("\n")
