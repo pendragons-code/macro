04e786cac5c94c25605bc76182fe929bbf390d40
Woon Seng Hong <88629349+pendragons-code@users.noreply.github.com>
2022-07-26 20:11:25 +0800

    Update main.py
    I think this should work, im doing this from phone so lmao????????

--- main.py
+++ main.py
@@ -14,44 +14,28 @@ print(Fore.CYAN+"""
 print(Fore.GREEN+"[1] Typewrite")
 print(Fore.GREEN+"[2] Keybinds")
 inputField = input("Please select an option: \n")
-
+s = input(Fore.BLUE+"Enter your message: ")
+x = input(Fore.BLUE+"How many times: ")
+t = input(Fore.BLUE+"Countdown: ")
+d = input(Fore.BLUE+"Delay per message: ")
+howManyX = int(x)
+tMinus = int(t)
+delay = int(d)
+while(tMinus != 0):
+    print("T-Minus "+str(tMinus))
+    sleep(1)
+    tMinus -= 1
 if inputField == "1":
-  s = input(Fore.BLUE+"Enter your message: ")
-  x = input(Fore.BLUE+"How many times: ")
-  t = input(Fore.BLUE+"Countdown: ")
-  d = input(Fore.BLUE+"Delay per message: ")
-  howManyX = int(x)
-  tMinus = int(t)
-  delay = int(d)
-
-  print(Fore.YELLOW+"MACRO IS STARTING...")
-
-  while(tMinus != 0):
-      print("T-Minus "+str(tMinus))
-      sleep(1)
-      tMinus -= 1   
-
-  for i in range(0, howManyX):
-    sleep(delay)
-    pyautogui.typewrite(s)
-    pyautogui.typewrite("\n")
+      for i in range(0, howManyX):
+        pyautogui.typewrite(s)
+        pyautogui.typewrite("\n")
+        sleep(delay)
 
 if inputField == "2":
-  print(Fore.YELLOW+"Have you copied the text? If not, copy it, then come back.")
-  x = input(Fore.BLUE+"How many times: ")
-  t = input(Fore.BLUE+"Countdown: ")
-  d = input(Fore.BLUE+"Delay per message: ")
-  howManyX = int(x)
-  tMinus = int(t)
-  delay = int(d)
-  print(Fore.YELLOW+"MACRO IS STARTING...")
-
-  while(tMinus != 0):
-      print("T-Minus "+str(tMinus))
-      sleep(1)
-      tMinus -= 1   
-  
-  for i in range(0, howManyX):
-    pyautogui.hotkey("ctrl","v")
-    pyautogui.typewrite("\n")
-    sleep(delay)
+  confirm = input(Fore.YELLOW+"Have you copied the text? If not, copy it, then come back.")
+    if confirm != "y" and confirm != "yes":
+        exit()
+      for i in range(0, howManyX):
+        pyautogui.hotkey("ctrl","v")
+        pyautogui.typewrite("\n")
+        sleep(delay)
